2daf1efbda40d5b88453bc5db0726b8e4f5b33f9
Moustapha Graies <moustaphagraies@gmail.com>
2023-02-05 22:54:16 +0100

    project init

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
+
+function createMainWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // Show devtools automatically if in development
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+
+  // Load a URL into the window.
+  win.loadFile("renderer/index.html");
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
