a60f4aaa9d1959865f2f1547fe7a5a0b8db980e8
Moustapha Graies <moustaphagraies@gmail.com>
2023-02-07 20:49:17 +0100

    created front-end page and calling api for the city name and timestamp

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,1 +1,32 @@
-console.log("test");
+const API_KEY = "435df22370b0a912956afed98585f747";
+const citySearchInput = document.getElementById("city-search-input");
+const searchButton = document.getElementById("search-button");
+const city = document.getElementById("city-name");
+const time = document.getElementById("time");
+
+searchButton.addEventListener("click", () => {
+  const cityName = citySearchInput.value;
+  getWeather(cityName);
+});
+
+function getWeather(cityName) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeather(data);
+    });
+}
+
+function displayWeather(data) {
+  city.innerHTML = `<h1 class="fw-bolder">${data.name}</h1>`;
+  const weatherTime = new Date(data.dt * 1000);
+  const formattedTime = weatherTime.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  console.log(formattedTime);
+  time.innerHTML = `<h3>${formattedTime}</h3>`;
+}
