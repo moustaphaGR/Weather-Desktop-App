3a68e92752b1d768f2b9cef41bcdedafba8506a8
Moustapha Graies <[moustaphagraies@gmail.com]>
2023-02-08 16:58:35 +0100

    display current weather of a city

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,7 +1,12 @@
+const API_KEY = "";
 const citySearchInput = document.getElementById("city-search-input");
 const searchButton = document.getElementById("search-button");
 const city = document.getElementById("city-name");
 const time = document.getElementById("time");
+const todayTemp = document.getElementById("todayTemp");
+const wind = document.getElementById("wind");
+const currentWeatherIcon = document.getElementById("currentWeatherIcon");
+console.log(currentWeatherIcon);
 
 searchButton.addEventListener("click", () => {
   const cityName = citySearchInput.value;
@@ -10,22 +15,36 @@ searchButton.addEventListener("click", () => {
 
 function getWeather(cityName) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
   )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       displayWeather(data);
-    });
+    })
+    .catch((err) => console.error(err));
 }
 
 function displayWeather(data) {
   city.innerHTML = `<h1 class="fw-bolder">${data.name}</h1>`;
-  const weatherTime = new Date(data.dt * 1000);
-  const formattedTime = weatherTime.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-  });
-  console.log(formattedTime);
+  const formattedTime = convertTime(data.timezone);
   time.innerHTML = `<h3>${formattedTime}</h3>`;
+  todayTemp.innerText = `${Math.floor(data.main.temp)}°`;
+  wind.innerText = `${Math.floor(data.wind.speed)} km/h`;
+  currentWeatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+}
+
+function convertTime(timezone) {
+  var now = new Date();
+  var timezoneOffset = timezone;
+  var time =
+    now.getTime() + now.getTimezoneOffset() * 60000 + timezoneOffset * 1000;
+  var convertedTime = new Date(time);
+  var hours = convertedTime.getHours();
+  var minutes = convertedTime.getMinutes();
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  var timeString =
+    (hours % 12 || 12) + ":" + minutes + " " + (hours < 12 ? "AM" : "PM");
+  return timeString;
 }
